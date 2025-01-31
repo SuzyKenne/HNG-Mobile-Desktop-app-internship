e39ffe74b31de5a13abd17010ea23dede7b089e7
Suzy Kenne <suzyndatewo@gmail.com>
2025-01-31 22:29:39 +0100

    Task-0 initial commit

---
+++ Task-0/app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>Hello suzy</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
